88f83e1a858eab3fe7c10955f1e38af8a778e3c0
mabadinakach <mabadinakach@gmail.com>
2021-03-29 16:25:45 -0600

    [-] TODO comments

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from random import randint
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
 def homepage():
     """Shows a greeting to the user."""
@@ -55,8 +53,3 @@ def dicegame():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-# TODO: Follow the assignment instructions to complete the required routes!
-# (And make sure to delete this TODO message when you're done!)
